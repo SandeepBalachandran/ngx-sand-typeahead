4b869843f246c8d56499be7f074f5f5a8ef66f11
Sandeep B <sandeepb@reflections.local>
2020-12-28 14:10:39 +0530

    More changes

--- projects/sand-typeahead/src/public-api.ts
+++ projects/sand-typeahead/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of sand-typeahead
  */
 
-export * from './lib/sand-typeahead.service';
-export * from './lib/sand-typeahead.component';
+export * from './lib/service/sand-typeahead.service';
+export * from './lib/ngx-sand-typeahead-component/sand-typeahead.component';
 export * from './lib/sand-typeahead.module';
+export * from './lib/pipes/limit-to.pipe'
